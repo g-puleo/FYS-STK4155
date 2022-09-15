5d51ae84ad7af30ff9b32bf73316c40eba9fe2b6
giammy00 <puleogianm@gmail.com>
2022-09-15 17:37:10 +0200

    reordering design matrix

--- PROJECT1/franke_fit.py
+++ PROJECT1/franke_fit.py
@@ -11,51 +11,56 @@ from random import random, seed
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import r2_score
+
+def MSE(z, ztilde):
+	return (1/len(z))*np.sum((z-ztilde)**2)
+
+def R2(z,ztilde):
+	return 1- (MSE(z,ztilde))/np.var(z)
+
+def FrankeFunction(x,y):
+	term1 = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2))
+	term2 = 0.75*np.exp(-((9*x+1)**2)/49.0 - 0.1*(9*y+1))
+	term3 = 0.5*np.exp(-(9*x-7)**2/4.0 - 0.25*((9*y-3)**2))
+	term4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
+	return term1 + term2 + term3 + term4
+
 fig = plt.figure()
 ax = fig.gca(projection='3d')
 # Make data.
-Nx = 1000
-Ny = 1000
+Nx = 100
+Ny = 100
 maxdegree = 5
 MSE_train_list = np.zeros(maxdegree+1)
 MSE_test_list = np.zeros(maxdegree+1)
 R2_train_list = np.zeros(maxdegree+1)
 R2_test_list = np.zeros(maxdegree+1)
+beta_matrix = np.zeros( ( (maxdegree+1)**2, maxdegree+1 ) )
+#generate x,y data from uniform distribution
+x = np.random.rand(Nx, 1)
+y = np.random.rand(Ny, 1)
+x, y = np.meshgrid(x,y)
 
 for degree in range(maxdegree+1):
-	x = np.random.rand(Nx, 1)
-	y = np.random.rand(Ny, 1)
-	x, y = np.meshgrid(x,y)
-
+	
 	#define own functions for MSE and R2 score
-	def MSE(z, ztilde):
-		return (1/len(z))*np.sum((z-ztilde)**2)
-
-	def R2(z,ztilde):
-		return 1- (MSE(z,ztilde))/np.var(z)
-
-	def FrankeFunction(x,y):
-		term1 = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2))
-		term2 = 0.75*np.exp(-((9*x+1)**2)/49.0 - 0.1*(9*y+1))
-		term3 = 0.5*np.exp(-(9*x-7)**2/4.0 - 0.25*((9*y-3)**2))
-		term4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
-		return term1 + term2 + term3 + term4
-
+	
 	#generate target data
-	z = (FrankeFunction(x, y) + 1*np.random.randn(Nx,Ny)).reshape(-1,1)
+	z = (FrankeFunction(x, y) + 0.1*np.random.randn(Nx,Ny)).reshape(-1,1)
 
 	#set up design matrix for a polynomial of given degree
 	X = np.zeros(( Nx*Ny, (degree+1)**2))
-	for ii in range(degree+1):
-		for jj in range(degree+1):
-			column_index = (degree+1)*ii+jj
-			X[:,column_index:column_index+1]  = (x**ii * y**jj).reshape(-1,1)
+	for S in range(degree+1):
+		for ii in range(S+1):
+			# need to figure out what the right column index is = (degree+1)*ii+jj
+			X[:,column_index:column_index+1]  = (x**ii * y**(S-ii)).reshape(-1,1)
 
 	#split data into train and test using scikitlearn
 	X_train, X_test, z_train, z_test = train_test_split(X,z, test_size=0.2)
 
 	#find optimal parameters using OLS
 	beta_opt = np.linalg.pinv(X_train.T @ X_train)@X_train.T @ z_train
+	beta_matrix[0:(degree+1)**2, degree:degree+1 ] = beta_opt
 	z_tilde_train = X_train @ beta_opt
 	z_tilde_test  = X_test @ beta_opt
 
@@ -73,6 +78,7 @@ for degree in range(maxdegree+1):
 # # Add a color bar which maps values to colors.
 # fig.colorbar(surf, shrink=0.5, aspect=5)
 # plt.show()
+#print(beta_matrix)
 fig, axs = plt.subplots(1,2)
 degrees_x = np.arange(maxdegree+1)
 axs[0].plot(degrees_x, MSE_train_list, label="train")
@@ -87,5 +93,15 @@ for ii in range(2):
 	axs[ii].set_xlabel("degree of polynomial")
 	axs[ii].grid(visible=True)
 	axs[ii].legend()
+
+fig_beta, axs_beta = plt.subplots(1,1)
+
+for jj in range(4):
+	axs_beta.plot( np.arange(maxdegree+1), beta_matrix[jj,:], label=f"$beta {jj}$")
+
+
+axs_beta.grid(visible=True)
+axs_beta.legend()
+	
 
 plt.show()
